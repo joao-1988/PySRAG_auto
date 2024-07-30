390ab20e607edbb67c6ce731e05c2a1a41437f67
João Flávio <42098112+joao-1988@users.noreply.github.com>
2024-07-30 01:13:51 -0300

    Update application.py

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ import pandas as pd
 from pysrag.data import SRAG
 from pysrag.model import GBMTrainer
 from joblib import dump, load
-from google.oauth2 import service_account
+#from google.oauth2 import service_account
 #from googleapiclient.discovery import build
 #from googleapiclient.errors import HttpError
 #from googleapiclient.http import MediaIoBaseDownload
