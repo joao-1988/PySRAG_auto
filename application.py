97b36e431cb99fbf60bdcd9508c5c3b1d6192542
João Flávio <42098112+joao-1988@users.noreply.github.com>
2024-07-30 01:04:44 -0300

    Update application.py

--- application.py
+++ application.py
@@ -5,11 +5,59 @@ import pandas as pd
 from pysrag.data import SRAG
 from pysrag.model import GBMTrainer
 from joblib import dump, load
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+from datetime import datetime
 
-# Treinando com mais de 1 arquivo (2 anos ou mais)
+# Função para baixar um arquivo
+def download_file(file_id, filepath, service):
+    try:
+        request = service.files().get_media(fileId=file_id)
+        with open(filepath, "wb") as f:
+            downloader = MediaIoBaseDownload(f, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print(f"Download {int(status.progress() * 100)}%.")
+    except HttpError as error:
+        print(f"An error occurred: {error}")
 
-path='./../bases'
+# Obter a data mais recente de cada ano
+def get_latest_data(all_files,string_year):
+  format_string = '%d-%m-%Y'
+  list_files = [i.replace(f'{string_year}-','').split('.')[0] for i in all_files if i[:8] == string_year]
+  latest_data = max(list_files,key=lambda x: datetime.strptime(x, format_string))
+  string_latest_data = string_year+'-'+latest_data+'.csv'
+  return string_latest_data
 
+# Configurar o caminho para as credenciais
+creds_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+creds = service_account.Credentials.from_service_account_file(creds_path)
+drive_service = build('drive', 'v3', credentials=creds)
+
+# Listar arquivos csv 
+results = drive_service.files().list(pageSize=1000).execute()
+items = results.get('files', [])
+all_files = [item['name'] for item in items if item['mimeType'] == 'text/csv']
+
+# Obter os arquivos mais recentes e seus respectivos ids
+influd22 = get_latest_data(all_files,'INFLUD22')
+influd23 = get_latest_data(all_files,'INFLUD23')
+influd24 = get_latest_data(all_files,'INFLUD24')
+print(influd22, influd23, influd24)
+influd22_id = [item['id'] for item in items if item['name'] == influd22][0]
+influd23_id = [item['id'] for item in items if item['name'] == influd23][0]
+influd24_id = [item['id'] for item in items if item['name'] == influd24][0]
+print(influd22_id, influd23_id, influd24_id)
+
+# Baixar os arquivos
+download_file(influd22_id,influd22,drive_service)
+download_file(influd23_id,influd23,drive_service)
+download_file(influd24_id,influd24,drive_service)
+
+# Carregar os dados
 cols_X = ['REGIAO_LATITUDE', 'REGIAO_LONGITUDE', 'UF_LATITUDE'
         , 'UF_LONGITUDE', 'LATITUDE', 'LONGITUDE', 'POPULACAO', 'IDADE_ANO'
         , 'ANO_SEM_SIN_PRI']
@@ -18,9 +66,7 @@ col_y = ['POS_SARS2', 'POS_FLUA', 'POS_FLUB', 'POS_VSR',
          'POS_ADENO', 'POS_METAP', 'POS_BOCA', 'POS_RINO', 'POS_OUTROS']
 demais_virus = ''
 
-list_filepath = ['https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SRAG/2022/INFLUD22-03-04-2023.csv' 
-,'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SRAG/2023/INFLUD23-15-07-2024.csv'
-,'https://s3.sa-east-1.amazonaws.com/ckan.saude.gov.br/SRAG/2024/INFLUD24-15-07-2024.csv']
+list_filepath = [influd22, influd23, influd24]
 
 list_X = []
 list_y = []
@@ -45,9 +91,11 @@ X = pd.concat(list_X).reset_index(drop=True)
 y = pd.concat(list_y).reset_index(drop=True)
 df_training_weeks = pd.concat(list_training_weeks).reset_index(drop=True)
 
+# Treinar o modelo
 trainer = GBMTrainer(objective='multiclass', eval_metric='multi_logloss')
 trainer.fit(X, y)
 
+# Salvar o modelo
 model = {'filename': list_filepath,
          'weeks': list_weeks,
          'train': list_train,
@@ -57,9 +105,9 @@ model = {'filename': list_filepath,
           'train_size': len(y),
           'best_boost_iteration': trainer.model.best_iteration_}
 
-#dump(model,'./dict_model')
+dump(model,'./dict_model')
 
-# Exclude recent lags
+# Exclui semanas com lag 0 e 1
 excl_weeks = []
 for lag in [0,1]:
   dict_lag = srag.get_start_day_of_week(lag)
@@ -69,14 +117,11 @@ ind_excl_weeks = df_training_weeks['ANO_SEM_SIN_PRI'].isin(excl_weeks)
 
 df_training_weeks_app = df_training_weeks[~ind_excl_weeks].query('SEM_SIN_PRI > 0')
 
+# Salvar os dados
 df_training_weeks_app.to_csv('./df_semanas.csv',index=False)
-
 SRAG.load_common_data().to_csv('./df_municipios.csv',index=False)
-
 feature_name = model['model'].model.feature_name_
 pd.Series(feature_name,name='feature_name').to_csv('./feature_name.csv',index=False)
-
 classes = model['virus']
 pd.Series(classes,name='virus').to_csv('./classes.csv',index=False)
-
 model['model'].model.booster_.save_model('./booster.txt')
