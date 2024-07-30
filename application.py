4d40d94cc36baf290435aa3e746c9191923c6492
João Flávio <42098112+joao-1988@users.noreply.github.com>
2024-07-30 01:11:57 -0300

    Update application.py

--- application.py
+++ application.py
@@ -6,8 +6,8 @@ from pysrag.data import SRAG
 from pysrag.model import GBMTrainer
 from joblib import dump, load
 from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaIoBaseDownload
+#from googleapiclient.discovery import build
+#from googleapiclient.errors import HttpError
+#from googleapiclient.http import MediaIoBaseDownload
 from datetime import datetime
 
