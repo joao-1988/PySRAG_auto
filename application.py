eb8e79e2e47326bd18784367aa46a78569a3c778
João Flávio <42098112+joao-1988@users.noreply.github.com>
2024-07-30 01:23:59 -0300

    Update application.py

--- application.py
+++ application.py
@@ -5,6 +5,11 @@ import pandas as pd
 from pysrag.data import SRAG
 from pysrag.model import GBMTrainer
 from joblib import dump, load
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+from datetime import datetime
 
 # Treinando com mais de 1 arquivo (2 anos ou mais)
 
